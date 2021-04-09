42ff589b6e78986d1e47a6fe676fbff395152701
Dmitriy <aquarikon23@gmail.com>
2021-04-09 13:52:00 +0800

    created conflict

--- codewars/ThePoetAndThePendulum.js
+++ codewars/ThePoetAndThePendulum.js
@@ -1,8 +1,3 @@
-function pendulum(values) {
-    let min = Math.min(...values);
-     values.splice(values.indexOf(min) , 1);
-     values.sort((a,b) => a - b );
-     let rigthArr = values.filter((el,i)=> i % 2 === 0);
-     let leftArr = values.filter((el,i) => i % 2 !== 0).reverse()
-     return [...leftArr , min, ...rigthArr]
-   }
+function minValue(values){
+  return +values.filter((el,i,arr) => i === arr.lastIndexOf(el)).sort((a,b) => a - b ).join('');
+}
