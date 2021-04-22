eb21ca6d8b2fdb17360a0ab59cf7886affb67185
Dmitriy <aquarikon23@gmail.com>
2021-04-22 09:19:26 +0800

    fixed the issues

--- Automation/test/specs/example.e2e.js
+++ Automation/test/specs/example.e2e.js
@@ -2,13 +2,9 @@ import LoginPage from  '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+    it('should login with valid credentials', () => {
+        browser.url("https://github.com");
+        console.log(browser.getTitle());
     });
 });
 
